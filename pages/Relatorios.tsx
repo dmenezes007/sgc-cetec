5df3194505fa7f40398300fc082629c54da71e0d
dev-caw <davison.menezes@caw.adv.br>
2025-10-22 21:05:32 -0300

    Primeiro commit: início do projeto

--- pages/Relatorios.tsx
+++ pages/Relatorios.tsx
@@ -189,7 +189,7 @@ const Relatorios: React.FC = () => {
                                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Data Fim</th>
                                     </tr>
                                 </thead>
-                                <tbody className.bg-white divide-y divide-gray-200">
+                                <tbody className="bg-white divide-y divide-gray-200">
                                     {filteredData.map((item) => (
                                         <tr key={item.id} className="hover:bg-gray-50">
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{item.servidor}</td>
